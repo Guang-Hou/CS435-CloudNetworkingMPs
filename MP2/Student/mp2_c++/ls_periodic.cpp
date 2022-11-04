324bc3e25c18fac5dbc40d3083e5aa476246fd8e
Guang-Hou <gfeng.hou@gmail.com>
2022-11-03 23:30:05 -0400

    ls_periodic only passes 1, 2, 6

---
+++ MP2/Student/mp2_c++/ls_periodic.cpp
@@ -0,0 +1,31 @@
+// link stage, sending single path LSA updates to neighbor
+
+#include "ls_periodic.hpp"
+#include <iostream>
+#include <thread>
+#include <pthread.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: %s mynodeid initialcostsfile logfile\n\n", argv[0]);
+        exit(1);
+    }
+
+    int inputId = atoi(argv[1]);
+    string costFile = argv[2];
+    string logFile = argv[3];
+
+    init(inputId, costFile, logFile);
+
+    pthread_t announcerThread;
+    pthread_create(&announcerThread, 0, announceToNeighbors, (void *)0);
+
+    pthread_t lsaThread;
+    pthread_create(&lsaThread, 0, announceLSA, (void *)0);
+
+    listenForNeighbors();
+
+    // testDij(6, 3);
+}
